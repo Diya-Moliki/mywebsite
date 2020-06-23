80bec93e97d61249229986d21df6df0155822979
Diya Moliki <59070900+Kwara1989@users.noreply.github.com>
2020-06-22 20:35:33 -0400

    Add files via upload

---
+++ mywebsite.py
@@ -0,0 +1,203 @@
+
+     
+  <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Front page</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="responsive.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <header class="header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="logo">
+                            <h2><a href="#">My Devops Bio</a></h2>
+                        </div>
+                    </div>
+                   
+            </div>
+        </header>
+        <section class="bg-text-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="bg-text">
+                               <h3>Becoming a DevOps Engineer</h3>
+                            <p> Breaking into DevOps can be very stressful as it requires continous improvement and learning of numerous softwares to stay grounded in the profession. My path to Devops was a bit rough but I had to stay devoted to acquiring knowledge and exploring different platforms to imbibe the culture. My career took off on the  system administration lane and I was very comfortable being a system administrator for a good number of years until the integration of development and operation team came into the limelight. Also, as companies move into cloud, I could clearly envision the end of my days as a sys-admin...this ongoing change prompted me five years ago to embark on the journey to become a devops engineer. Currently, I am an experienced Devops and Cloud computing professional with an extensive knowledge of cloud computing technology, software performance monitoring and automation of  builds, tests and deployment  </p> <p> Diya Moliki BSc. AWS SA, CSM, Security+, LPIC, Linux+ </p> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="blog-post-area">
+            <div class="container">
+                <div class="row">
+                    <div class="blog-post-area-style">
+                            <div class="col-md-12">
+                                <div class="single-post-big">
+                                    <div class="big-image">
+                                        
+<img src="https://diyamolimages.s3.amazonaws.com/IMG_3964.JPG" width="320" height="270">
+                                    </div>
+                                    <div class="big-text">
+                                        <h3><a href="#">My Professional Summary</a></h3>
+                                        <p> I presently work with Chatscrum inc where I perform automation of on-prem/cloud application deployment into production, Jenkins CI/CD pipeline, implementation of effective infrastructure as Code concept with Terraform, Agile methodology, Configuration management with Ansible, source code management with Git, containerizing application via microservice platform "Docker", System monitoring with Splunk, Vulnerabilty Check and System audit with Openscap and Container orchestration with Kubernetes.  
+</p>
+                                        <h4><span class="date">20 March 2020</span><span class="author">Posted By: <span class="author-name">Diya Moliki</span></span>
+    
+                                    </h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="single-post">
+                                    <img src="https://diyamolimages.s3.amazonaws.com/screen.jpg" width="230" height="158" alt="">
+                                    <h3><a href="https://www.docker.com/">Docker</a></h3>
+                                    <h4><span class="author-name">Containerization</span></span>
+                                    </h4>
+                                    <p>Docker makes it easy to develop, deploy, and run applications within containers in a production environment. I continiously configure docker to containerized and run certain scrum application using docker text file. </p>
+                                    <h4><span>4 Years Experience</span></h4>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="single-post">
+                                    <img src="https://diyamolimages.s3.amazonaws.com/spp.jpg" width="230" height="158" alt="">
+                                    <h3><a href="#">Splunk</a></h3>
+                                    <h4><span class="author-name">Monitoring</span></span>
+                                    </h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed dotempor incididunt ut. labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                                    <h4><span>25 February 2017</span></h4>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="single-post">
+                                    <img src="https://diyamolimages.s3.amazonaws.com/Ans.jpg" width="230" height="158" alt="">
+                                    <h3><a href="#">Ansible</a></h3>
+                                    <h4><span class="author-name">Configuration Management</span></span>
+                                    </h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed dotempor incididunt ut. labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                                    <h4><span>25 February 2017</span></h4>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="single-post">
+                                    <img src="https://diyamolimages.s3.amazonaws.com/kube.jpg" width="230" height="158" alt="">
+                                    <h3><a href="#">Kubernetes</a></h3>
+                                    <h4><span class="author-name">Container Orchestration</span></span>
+                                    </h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed dotempor incididunt ut. labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                                    <h4><span>25 February 2017</span></h4>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="single-post">
+                                    <img src="img/post-image6.jpg" alt="">
+                                    <h3><a href="#">OpenScap</a></h3>
+                                    <h4><span class="author-name">System Auditing</span></span>
+                                    </h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed dotempor incididunt ut. labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                                    <h4><span>25 February 2017</span></h4>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="single-post">
+                                    <img src="img/post-image7.jpg" alt="">
+                                    <h3><a href="#">Vagrant</a></h3>
+                                    <h4><span class="author-name">VM's Automation</span></span>
+                                    </h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed dotempor incididunt ut. labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                                    <h4><span>25 February 2017</span></h4>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="single-post">
+                                    <img src="img/post-image8.jpg" alt="">
+                                    <h3><a href="#">Jenkins</a></h3>
+                                    <h4><span class="author-name">CI/CD Pipeline</span></span>
+                                    </h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed dotempor incididunt ut. labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                                    <h4><span>25 February 2017</span></h4>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="single-post">
+                                    <img src="img/post-image9.jpg" alt="">
+                                    <h3><a href="#">Platforms: AWS + Linux OS</a></h3>
+                                    <h4> <span class="author-name">Cloud + OS</span></span>
+                                    </h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed dotempor incididunt ut. labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                                    <h4><span>25 February 2017</span></h4>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
+            <div class="pegination">
+                <!--
+                <ul>
+                    <li><i class="fa fa-angle-left" aria-hidden="true"></i></li>
+                    <li class="active">1</li>
+                    <li>2</li>
+                    <li>3</li>
+                    <li><i class="fa fa-angle-right" aria-hidden="true"></i></li>
+                </ul>
+-->
+
+                <div class="nav-links">
+                    <span class="page-numbers current">D</span>
+                    <a class="page-numbers" href="#">I</a>
+                    <a class="page-numbers" href="#">Y</a>
+                    <a class="page-numbers" href="#">A</a>
+     
+                   
+                </div>
+            </div>
+        </section>
+        <footer class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="footer-bg">
+                            <div class="row">
+                                <div class="col-md-9">
+                                    <div class="footer-menu">
+                                        <ul>
+                                            <li class="active"><a href="#">designed by diya moliki</a></li>
+                                          
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="footer-icon">
+                                        <p><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a><a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a></p>
+                                    </div>
+                                </div>
+                            </div> .
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/active.js"></script>
+</body>
+
+</html>
+
